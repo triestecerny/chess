27c904ac4513465f1563efa59065f82bf5c9c042
Trieste Cerny <triestecerny@gmail.com>
2026-03-02 13:35:11 -0700

    Delete End point works!

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,34 @@
 package server;
 
-import io.javalin.*;
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+import service.ClearService;
+import io.javalin.Javalin;
 
 public class Server {
 
     private final Javalin javalin;
+    private final ClearService clearService;
+    private final Gson gson = new Gson();
 
     public Server() {
+        MemoryDataAccess dataAccess = new MemoryDataAccess();
+        clearService = new ClearService(dataAccess);
+
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        // Register your endpoints and exception handlers here.
+        // Register endpoints here
+        javalin.delete("/db", this::clear);
+    }
 
+    private void clear(io.javalin.http.Context ctx) {
+        try {
+            clearService.clear();
+            ctx.status(200).result("{}");
+        } catch (DataAccessException e) {
+            ctx.status(500).result(gson.toJson(new ErrorResponse("Error: " + e.getMessage())));
+        }
     }
 
     public int run(int desiredPort) {
@@ -21,4 +39,6 @@ public class Server {
     public void stop() {
         javalin.stop();
     }
+
+    private record ErrorResponse(String message) {}
 }
