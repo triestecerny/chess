78e607dc7099c042ce6f334f4093f0655f1806d2
Trieste Cerny <triestecerny@gmail.com>
2026-03-02 15:22:24 -0700

    register endpoint works!

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -4,22 +4,26 @@ import com.google.gson.Gson;
 import dataaccess.DataAccessException;
 import dataaccess.MemoryDataAccess;
 import service.ClearService;
+import service.UserService;
 import io.javalin.Javalin;
 
 public class Server {
 
     private final Javalin javalin;
     private final ClearService clearService;
+    private final UserService userService;
     private final Gson gson = new Gson();
 
     public Server() {
         MemoryDataAccess dataAccess = new MemoryDataAccess();
         clearService = new ClearService(dataAccess);
+        userService = new service.UserService(dataAccess);
 
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register endpoints here
         javalin.delete("/db", this::clear);
+        javalin.post("/user", this::register);
     }
 
     private void clear(io.javalin.http.Context ctx) {
@@ -30,7 +34,26 @@ public class Server {
             ctx.status(500).result(gson.toJson(new ErrorResponse("Error: " + e.getMessage())));
         }
     }
+    private void register(io.javalin.http.Context ctx) {
+        try {
+            model.UserData user = gson.fromJson(ctx.body(), model.UserData.class);
 
+            model.AuthData auth = userService.register(user);
+
+            ctx.status(200).result(gson.toJson(auth));
+
+        } catch (DataAccessException e) {
+
+            if (e.getMessage().contains("bad request")) {
+                ctx.status(400);
+            } else if (e.getMessage().contains("already taken")) {
+                ctx.status(403);
+            } else {
+                ctx.status(500);
+            }
+            ctx.result(gson.toJson(new ErrorResponse(e.getMessage())));
+        }
+    }
     public int run(int desiredPort) {
         javalin.start(desiredPort);
         return javalin.port();
