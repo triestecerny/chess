cb7e2c1c867392a9e99e857856e5a3960f3214a5
Trieste Cerny <triestecerny@gmail.com>
2026-03-04 15:14:58 -0700

    1 positive test for ClearServiceTest

--- server/src/main/java/service/ClearServiceTest.java
+++ server/src/main/java/service/ClearServiceTest.java
@@ -1,4 +1,20 @@
 package service;
 
+import dataaccess.MemoryDataAccess;
+import dataaccess.DataAccessException;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ClearServiceTest {
+
+    @Test
+    void clearPositive() throws DataAccessException {
+        var dao = new MemoryDataAccess();
+        dao.createUser(new model.UserData("bob", "pass", "email"));
+
+        var service = new ClearService(dao);
+        service.clear();
+
+        assertNull(dao.getUser("bob"));
+    }
 }
