884c3b4ce064745309d2f5f263f72b5a5e6e28d3
Trieste Cerny <triestecerny@gmail.com>
2026-03-20 15:28:11 -0600

    Prelogin UI works to at least register

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,7 @@
 package client;
 
-import chess.*;
-
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        new Repl(8080).run();
     }
 }
---
+++ client/src/main/java/client/PreloginUI.java
@@ -0,0 +1,54 @@
+package client;
+
+public class PreloginUI {
+
+    private final ServerFacade facade;
+
+    public PreloginUI(ServerFacade facade) {
+        this.facade = facade;
+    }
+
+    private String help() {
+        return """
+                - register <USERNAME> <PASSWORD> <EMAIL> - to create an account
+                - login <USERNAME> <PASSWORD> - to play chess
+                - quit - playing chess
+                - help - with possible commands
+                """;
+    }
+    public String eval(String input) {
+        var tokens = input.split(" ");
+        var cmd = tokens[0];
+        return switch (cmd) {
+            case "help" -> help();
+            case "quit" -> "Goodbye!";
+            case "register" -> register(tokens);
+            case "login" -> login(tokens);
+            default -> "Unknown command. Type 'help' for options.";
+        };
+    }
+
+    private String register(String[] tokens) {
+        if (tokens.length < 4) {
+            return "Usage: register <USERNAME> <PASSWORD> <EMAIL>";
+        }
+        try {
+            var authData = facade.register(tokens[1], tokens[2], tokens[3]);
+            return "Registered and logged in as " + authData.username();
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+
+    private String login(String[] tokens) {
+        if (tokens.length < 3) {
+            return "Usage: login <USERNAME> <PASSWORD>";
+        }
+        try {
+            var authData = facade.login(tokens[1], tokens[2]);
+            return "Logged in as " + authData.username();
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+}
