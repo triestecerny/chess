e87736d24d786d0ab450936ca9b7d2fb6897fee5
Trieste Cerny <triestecerny@gmail.com>
2026-03-20 12:58:12 -0600

    request server

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,4 +1,15 @@
 package client;
 
+import com.google.gson.Gson;
+import java.io.*;
+import java.net.*;
+
 public class ServerFacade {
+
+    private final String serverUrl;
+    private final Gson gson = new Gson();
+
+    public ServerFacade(int port) {
+        this.serverUrl = "http://localhost:" + port;
+    }
 }
