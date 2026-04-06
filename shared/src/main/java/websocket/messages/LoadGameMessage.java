983a1bcb27884eb3386165b08cb19b403b16ee2d
Trieste Cerny <triestecerny@gmail.com>
2026-04-06 16:36:14 -0600

    adding messages

---
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -0,0 +1,16 @@
+package websocket.messages;
+
+import model.GameData;
+
+public class LoadGameMessage extends ServerMessage {
+    private final GameData game;
+
+    public LoadGameMessage(GameData game) {
+        super(ServerMessageType.LOAD_GAME);
+        this.game = game;
+    }
+
+    public GameData getGame() {
+        return game;
+    }
+}
